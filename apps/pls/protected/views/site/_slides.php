7916d3eccaa8f80825e51b96ec6d0608b82fc70d
tunjiebifemi <tunjiebifemi@gmail.com>
2022-10-29 23:37:32 -0400

    Replaced Slides Content

--- apps/pls/protected/views/site/_slides.php
+++ apps/pls/protected/views/site/_slides.php
@@ -23,46 +23,28 @@
 					More</a>
 			</div>
 		</div>
+		<!-- 3 -->
 		<div class="swiper-slide">
 			<div class="overlay"></div>
 			<div class="content">
 				<i class="fal fa-comment-alt-edit"></i>
 				<h3>SuperEval’s latest products updates include:</h3>
 				<div class="row is-flex">
-					<div class="col-md-6">
-						<div class="bubble">1. Show evaluator's previous evaluation data.</div>
+					<div class="col-md-12">
+						<!-- start feedwind code -->
+						<script type="text/javascript" src="https://feed.mikle.com/js/fw-loader.js" preloader-text="Loading" data-fw-param="157725/"></script>
+						<!-- end feedwind code -->
 					</div>
-					<div class="col-md-6">
-						<div class="bubble">2. Added consolidated data view with highest rated, lowest rated, etc.
-							to all roles.
-						</div>
-					</div>
-					<div class="col-md-6">
-						<div class="bubble">3. Added competency preview links to consolidated review table.
-						</div>
-					</div>
-					<div class="col-md-6">
-						<div class="bubble">4. Send survey email to evaluatee on signature; show evaluator survey
-							dialog on signature.
-						</div>
-					</div>
+					
 				</div>
-				<a href="https://supereval.com/blog/supereval-recent-platform-updates" target="_blank"
-				   class="btn btn-primary">Read
-					More</a>
 			</div>
 		</div>
+		<!-- 4 -->
 		<div class="swiper-slide">
 			<div class="overlay"></div>
 			<div class="content">
 				<i class="fal fa-comment-alt-lines"></i>
-				<h3>Six Benefits of Having an Open Communication System with Teachers and School Staff</h3>
-				<div class="row">
-					<div class="col-md-12 bubble">
-						This communication system has a number of perks. Here are six ways your school district may be
-						able to benefit from it.
-					</div>
-				</div>
+				<!-- start feedwind code --> <script type="text/javascript" src="https://feed.mikle.com/js/fw-loader.js" preloader-text="Loading" data-fw-param="157726/"></script> <!-- end feedwind code -->
 				<a href="https://supereval.com/blog/open-communication-system" target="_blank" class="btn btn-primary">Read
 					Our Blog</a>
 			</div>
